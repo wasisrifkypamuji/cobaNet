e16f6adbd56de403bd77e03a9d190240c01bc014
wasisrifkypamuji <pamujirifkywasis@gmail.com>
2024-10-19 13:22:16 +0700

    comit master

--- kalkulator/Form1.cs
+++ kalkulator/Form1.cs
@@ -14,7 +14,7 @@ namespace kalkulator
 
         private void TAMBAH_Click(object sender, EventArgs e)
         {
-            double a = 0, b = 0, c;
+            double a = 1, b = 6, c;
 
             a = Convert.ToDouble(A.Text);
             b = Convert.ToDouble(B.Text);
